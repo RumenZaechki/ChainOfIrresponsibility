0e070a9d6d02cf3b1c55a904329d004fb47f2721
Rumen Zaechki <mation_palaeo@aleeas.com>
2023-07-28 21:53:25 +0300

    Initial commit

---
+++ ChainOfIrresponsibility/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ChainOfIrresponsibility
+{
+    public class Configuration<TRequest>
+    {
+        private readonly IServiceCollection _services;
+
+        public Configuration(IServiceCollection services)
+        {
+            _services = services;
+        }
+
+        public Configuration<TRequest> WithHandler<THandler>()
+        {
+            return null;
+        }
+    }
+}
---
+++ ChainOfIrresponsibility/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ChainOfIrresponsibility
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static Configuration<TRequest> IncludeChain<TRequest>(this IServiceCollection services)
+        {
+            return null;
+        }
+    }
+}
